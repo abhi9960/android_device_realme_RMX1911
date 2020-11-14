0a54bc983c8106a714ec793ba02fb20b83b6f5a3
Abhishek Raut <61415942+abhi9960@users.noreply.github.com>
2020-11-14 15:05:59 +0530

    initial bringup for rmx1925

--- aosp_RMX1925.mk
+++ aosp_RMX1925.mk
@@ -4,15 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, device/realme/RMX1911/device.mk)
+$(call inherit-product, device/realme/RMX1925/device.mk)
 $(call inherit-product, vendor/aosp/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_RMX1911
-PRODUCT_DEVICE := RMX1911
+PRODUCT_NAME := aosp_RMX1925
+PRODUCT_DEVICE := RMX1925
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := Realme 5
+PRODUCT_MODEL := Realme 5s
 PRODUCT_MANUFACTURER := realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
